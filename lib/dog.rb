419bf5520a54f642597de30b3c6b4259901d414c
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-11 21:10:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Dog
+  # attr_accessor :bark
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(bark)
+    @bark = bark
+  end
+
+  def bark
+    puts "woof!"
+    @bark
+  end
+end
+fido = Dog.new
+# fido.bark = "woof!"
+# fido.bark
