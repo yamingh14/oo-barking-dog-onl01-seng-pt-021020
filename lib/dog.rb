2339ea47172eeb4d0549b7006b6e534ce3571e4e
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-11 21:06:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,19 @@
 class Dog
   attr_accessor :bark
 
-  def initialize
-    @bark = fido
-  end
-
   def name=(name)
     @name = name
   end
 
   def name
     @name
+  end
+
+  def bark(bark)
+    @bark
+  end
+
+  def bark
+    @bark
   end
 end
